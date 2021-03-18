70c4847cf98572e91ed1e707e246a5445d533c37
lubos <lubos31262@gmail.com>
2021-03-18 08:52:37 +0100

    test exchange rate

--- price.py
+++ price.py
@@ -1,5 +1,11 @@
  #!/usr/bin/env python3
+
+
 from exchangeratesapi import Api
 
 if __name__ == '__main__':
-	pass  
+	api = Api()
+
+	# Get the latest foreign exchange rates:
+	api.get_rates()
+	print(api.get_rates(start_date="2018-03-26"))
