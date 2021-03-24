96b201ac97fe5244ef935f29421b64427351c69b
lubos <lubos31262@gmail.com>
2021-03-24 19:45:25 +0100

    price-fc - first complete draft

--- price.py
+++ price.py
@@ -7,17 +7,18 @@ import json
 import redis
 import rejson
 import logging
-
+import isodate
 
 VERBOSE         = 0
 cache           = redis.Redis(host='cache', port=6379)
-
+#############################################################################
+#############################################################################
+#############################################################################
 def price_to_eur(currency="EUR", price=0):
     if currency == "EUR":
 	    return price
     if price is None:
 	    return None
-
     try:
         currency_api = Api()
         # IF the currency is not known
@@ -30,7 +31,60 @@ def price_to_eur(currency="EUR", price=0):
         logging.warning("External service exchangeratesapi failed in price-fc.")
         return None
     return round(rate * price)
+#############################################################################
+#############################################################################
+#############################################################################
+def extract_data_from_cache(
+        request_id,
+        offer_level_items,
+        tripleg_level_items):
 
+    output_offer_level_items   = {}
+    output_tripleg_level_items = {}
+
+    offer_ids  = cache.jsonget('{}:offers'.format(request_id))
+    #offer_ids = cache.lrange('{}:offers'.format(request_id), 0, -1)
+
+    output_offer_level_items["offer_ids"] = offer_ids
+    if offer_ids  is not None:
+        for offer in offer_ids:
+            if VERBOSE == 1:
+                print("offer = " + offer)
+            output_offer_level_items[offer] = {}
+            if len(tripleg_level_items) > 0:
+                output_tripleg_level_items[offer] = {}
+            for offer_level_item in offer_level_items:
+                # assembly key for offer level
+                temp_key  = request_id + ":" + offer + ":" + offer_level_item
+                # extract offer level data from cache
+                temp_data = cache.jsonget(temp_key)
+                # temp_data = cache.get(temp_key)
+                output_offer_level_items[offer][offer_level_item] = temp_data
+                if temp_data is not None:
+                    if VERBOSE == 1:
+                        print(offer_level_item + " = " + str(temp_data))
+                else:
+                    if VERBOSE == 1:
+                        print(offer_level_item + " = None")
+                # extract information at the tripleg level
+                if len(tripleg_level_items) > 0:
+                    temp_key      = request_id + ":" + offer + ":" + "legs"
+                    tripleg_ids   = cache.jsonget(temp_key)
+                    #tripleg_ids  = cache.get(temp_key)
+
+                    output_tripleg_level_items[offer]["triplegs"] = tripleg_ids
+                    for tripleg_id in tripleg_ids:
+                        print(tripleg_id)
+                        output_tripleg_level_items[offer][tripleg_id] = {}
+                        for tripleg_level_item in tripleg_level_items:
+                            temp_key = request_id + ":" + offer + ":" + tripleg_id + ":" + tripleg_level_item
+                            tripleg_data   = cache.jsonget(temp_key)
+                            #tripleg_data  = cache.get(temp_key)
+                            output_tripleg_level_items[offer][tripleg_id][tripleg_level_item] = tripleg_data
+    return output_offer_level_items, output_tripleg_level_items
+#############################################################################
+#############################################################################
+#############################################################################
 
 app = Flask(__name__)
 @app.route('/compute', methods=['POST'])
@@ -49,88 +103,85 @@ def extract():
         print("request_id = " + request_id)
         print("______________________________")
 
-    # extract required offer data from cache
-    offer_ids            = []
-    offer_currency       = {}
+    # I. extract data required by price-fc from cache
+    output_offer_level, output_tripleg_level = extract_data_from_cache(
+        request_id,
+        ["currency", "bookable_total", "complete_total"],
+        ["duration", "can_share_cost"])
+
+    if VERBOSE== 1:
+        print("output_offer_level   = " + str(output_offer_level))
+        print("output_tripleg_level = " + str(output_tripleg_level))
+
+    # II. use the external service to convert prices to EUR
+    for offer in output_offer_level["offer_ids"]:
+        # complete_total
+        output_offer_level[offer]["complete_total"] = price_to_eur(output_offer_level[offer]["currency"], output_offer_level[offer]["complete_total"])
+        # boobkable_total
+        output_offer_level[offer]["bookable_total"] = price_to_eur(output_offer_level[offer]["currency"], output_offer_level[offer]["bookable_total"])
+
+    # III. compute values assigned to price-fc
+    # compute ticket_coverage and prepare data for calculation of scores
+    ticket_coverage      = {}
     offer_bookable_total = {}
     offer_complete_total = {}
+    for offer in output_offer_level["offer_ids"]:
+        offer_bookable_total[offer] = output_offer_level[offer]["bookable_total"]
+        offer_complete_total[offer] = output_offer_level[offer]["complete_total"]
+        if (output_offer_level[offer]["bookable_total"] is not None) and (output_offer_level[offer]["complete_total"] is not None) and (output_offer_level[offer]["complete_total"] > 0):
+            ticket_coverage[offer] = output_offer_level[offer]["bookable_total"]/output_offer_level[offer]["complete_total"]
 
-    offer_data_1 = cache.jsonget('{}:offers'.format(request_id))
-    #offer_data_1 = cache.lrange('{}:offers'.format(request_id), 0, -1)
+    # process can_share_cost (aggregate can_share_cost over triplegs using duration information)
+    can_share_cost = {}
+    for offer in output_offer_level["offer_ids"]:
+        triplegs = output_tripleg_level[offer]["triplegs"]
+        temp_duration = {}
+        temp_can_share_cost = {}
+        for tripleg in triplegs:
+            ttemp_duration                = output_tripleg_level[offer][tripleg]["duration"]
+            ttemp_can_share_cost          = output_tripleg_level[offer][tripleg]["can_share_cost"]
+            temp_duration[tripleg]        = isodate.parse_duration(ttemp_duration).seconds
+            temp_can_share_cost[tripleg]  = ttemp_can_share_cost
+        can_share_cost[offer] = normalization.aggregate_a_quantity_over_triplegs(triplegs, temp_duration, temp_can_share_cost)
 
-    if not(offer_data_1 is None):
-        for offer in offer_data_1:
-            if VERBOSE == 1:
-                print("offer = " + offer)
-            key_currency       = request_id + ":" + offer + ":" + "currency"
-            key_bookable_total = request_id + ":" + offer + ":" + "bookable_total"
-            key_complete_total = request_id + ":" + offer + ":" + "complete_total"
-            currency           = cache.jsonget(key_currency)
-            #currency = cache.get(key_currency)
-            if VERBOSE == 1:
-                if currency is not None:
-                    print("currency = " + currency)
-                else:
-                    print("currency = None")
 
-            bookable_total = cache.jsonget(key_bookable_total)
-            #bookable_total = cache.get(key_bookable_total)
-            if VERBOSE == 1:
-                if bookable_total is not None:
-                    print("bookable_total = " + str(bookable_total))
-                else:
-                    print("bookable_total = None")
-
-            complete_total = cache.jsonget(key_complete_total)
-            #complete_total = cache.get(key_complete_total)
-            if VERBOSE == 1:
-                if complete_total is not None:
-                    print("complete_total = " + str(complete_total))
-                else:
-                    print("complete_total = None")
-            offer_ids.append(offer)
-            offer_currency[offer]         = currency
-            offer_bookable_total[offer]   = price_to_eur(currency, bookable_total)
-            offer_complete_total[offer]   = price_to_eur(currency, complete_total)
-
-            if VERBOSE == 1:
-                print("offer_ids            = " + str(offer_ids))
-                print("offer_currency       = " + str(offer_currency))
-                print("offer_bookable_total = " + str(offer_bookable_total))
-                print("offer_complete_total = " + str(offer_complete_total))
-
-	# Compute outputs
-    ticket_coverage      = {}
-
-    for offer_id in offer_bookable_total:
-        if (offer_bookable_total[offer_id] is not None) and (offer_complete_total[offer_id] is not None) and (offer_complete_total[offer_id] > 0):
-            ticket_coverage[offer_id] = offer_bookable_total[offer_id]/offer_complete_total[offer_id]
-
-	#apply aggregation at the trip leg level
-	# TODO
-
-	# apply normalization at the offer level
-    offer_complete_total_z_scores        = normalization.zscore(offer_complete_total)
-    offer_bookable_total_z_scores        = normalization.zscore(offer_bookable_total)
+    # calculate zscores
+    offer_complete_total_z_scores        = normalization.flipped_zscore(offer_complete_total)
     ticket_coverage_z_scores             = normalization.zscore(ticket_coverage)
-    offer_complete_total_minmax_scores   = normalization.minmaxscore(offer_complete_total)
-    offer_bookable_total_minmax_scores   = normalization.minmaxscore(offer_bookable_total)
-    ticket_coverage_minmax_scores        = normalization.minmaxscore(ticket_coverage)
+    can_share_cost_z_scores              = normalization.zscore(can_share_cost)
+
+    # calculate minmaxscores
+    #offer_complete_total_minmax_scores   = normalization.flipped_minmaxscore(offer_complete_total)
+    #ticket_coverage_minmax_scores        = normalization.minmaxscore(ticket_coverage)
+    #can_share_cost_minmax_scores         = normalization.minmaxscore(can_share_cost)
 
     if VERBOSE == 1:
-	    print("offer_complete_total_z_scores = " + str(offer_complete_total_z_scores))
-	    print("offer_bookable_total_z_scores = " + str(offer_bookable_total_z_scores))
-	    print("ticket_coverage_zscores  = " + str(ticket_coverage_z_scores ))
-	    print("offer_complete_total_minmax_scores = " + str(offer_complete_total_minmax_scores))
-	    print("offer_bookable_total_minmax_scores = " + str(offer_bookable_total_minmax_scores))
-	    print("ticket_coverage_minmax_scores  = " + str(ticket_coverage_minmax_scores ))
-	    print("price-fc end")
+        print("offer_complete_total_z_scores      = " + str(offer_complete_total_z_scores))
+        print("ticket_coverage_zscores            = " + str(ticket_coverage_z_scores ))
+        print("can_share_cost_zscores             = " + str(can_share_cost_z_scores))
 
-	# Store outputs to cache
-    # TODO
+        #print("offer_complete_total_minmax_scores = " + str(offer_complete_total_minmax_scores))
+        #print("ticket_coverage_minmax_scores      = " + str(ticket_coverage_minmax_scores))
+        #print("can_share_cost_minmax_scores       = " + str(can_share_cost_minmax_scores))
 
+
+    # IV. store the results produced by price-fc to cache
+    "total_price"
+    for offer in offer_complete_total_z_scores:
+        temp_key = request_id + ":" + offer + ":total_price"
+        cache.set(temp_key, offer_complete_total_z_scores[offer])
+    "ticket_coverage"
+    for offer in ticket_coverage_z_scores:
+        temp_key = request_id + ":" + offer + ":ticket_coverage"
+        cache.set(temp_key, ticket_coverage_z_scores[offer])
+    "can_share_cost"
+    for offer in can_share_cost_z_scores:
+        temp_key = request_id + ":" + offer + ":can_share_cost"
+        cache.set(temp_key, can_share_cost_z_scores[offer])
     return response
-
+#############################################################################
+#############################################################################
+#############################################################################
 
 if __name__ == '__main__':
     import os
@@ -146,7 +197,6 @@ if __name__ == '__main__':
 
     print("launching FLASK APP")
     app.run(port=FLASK_PORT, debug=True)
-
     exit(0)
 
 
