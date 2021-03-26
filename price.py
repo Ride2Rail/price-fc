42820ab3c4a51f1abf75b949fcd7df6cfa7be9f0
lubos <lubos31262@gmail.com>
2021-03-26 09:41:28 +0100

    - logging and conf added - work with cache separated

--- price.py
+++ price.py
@@ -1,16 +1,50 @@
 #!/usr/bin/env python3
-
-from flask            import Flask, request, abort
-from r2r_offer_utils  import normalization
-from exchangeratesapi import Api
-import json
-import redis
-import rejson
+import os
+import sys
+import pathlib
 import logging
+import configparser as cp
+
+from r2r_offer_utils  import normalization
+from r2r_offer_utils  import cache_operations
+from exchangeratesapi import Api
 import isodate
 
-VERBOSE         = 0
+from flask            import Flask, request, abort
+import redis
+import rejson
+import json
+
+
 cache           = redis.Redis(host='cache', port=6379)
+
+##### Config
+service_basename = os.path.splitext(os.path.basename(__file__))[0]
+config_file = '{name}.conf'.format(name=service_basename)
+config = cp.ConfigParser()
+config.read(config_file)
+#####
+
+##### Logging
+# create logger
+logger = logging.getLogger(service_basename)
+logger.setLevel(logging.DEBUG)
+# create formatter
+formatter_fh = logging.Formatter('[%(asctime)s][%(levelname)s]: %(message)s')
+formatter_ch = logging.Formatter('[%(asctime)s][%(levelname)s](%(name)s): %(message)s')
+default_log = pathlib.Path(config.get('logging', 'default_log'))
+try:
+    default_log.parent.mkdir(parents=True, exist_ok=True)
+    default_log.touch(exist_ok=True)
+
+    basefh = logging.FileHandler(default_log, mode='a+')
+except Exception as err:
+    print("WARNING: could not create log file '{log}'"
+          .format(log=default_log), file=sys.stderr)
+    print("WARNING: {err}".format(err=err), file=sys.stderr)
+#####
+
+VERBOSE = int(str(pathlib.Path(config.get('running', 'verbose'))))
 #############################################################################
 #############################################################################
 #############################################################################
@@ -34,57 +68,6 @@ def price_to_eur(currency="EUR", price=0):
 #############################################################################
 #############################################################################
 #############################################################################
-def extract_data_from_cache(
-        request_id,
-        offer_level_items,
-        tripleg_level_items):
-
-    output_offer_level_items   = {}
-    output_tripleg_level_items = {}
-
-    offer_ids  = cache.jsonget('{}:offers'.format(request_id))
-    #offer_ids = cache.lrange('{}:offers'.format(request_id), 0, -1)
-
-    output_offer_level_items["offer_ids"] = offer_ids
-    if offer_ids  is not None:
-        for offer in offer_ids:
-            if VERBOSE == 1:
-                print("offer = " + offer)
-            output_offer_level_items[offer] = {}
-            if len(tripleg_level_items) > 0:
-                output_tripleg_level_items[offer] = {}
-            for offer_level_item in offer_level_items:
-                # assembly key for offer level
-                temp_key  = request_id + ":" + offer + ":" + offer_level_item
-                # extract offer level data from cache
-                temp_data = cache.jsonget(temp_key)
-                # temp_data = cache.get(temp_key)
-                output_offer_level_items[offer][offer_level_item] = temp_data
-                if temp_data is not None:
-                    if VERBOSE == 1:
-                        print(offer_level_item + " = " + str(temp_data))
-                else:
-                    if VERBOSE == 1:
-                        print(offer_level_item + " = None")
-                # extract information at the tripleg level
-                if len(tripleg_level_items) > 0:
-                    temp_key      = request_id + ":" + offer + ":" + "legs"
-                    tripleg_ids   = cache.jsonget(temp_key)
-                    #tripleg_ids  = cache.get(temp_key)
-
-                    output_tripleg_level_items[offer]["triplegs"] = tripleg_ids
-                    for tripleg_id in tripleg_ids:
-                        print(tripleg_id)
-                        output_tripleg_level_items[offer][tripleg_id] = {}
-                        for tripleg_level_item in tripleg_level_items:
-                            temp_key = request_id + ":" + offer + ":" + tripleg_id + ":" + tripleg_level_item
-                            tripleg_data   = cache.jsonget(temp_key)
-                            #tripleg_data  = cache.get(temp_key)
-                            output_tripleg_level_items[offer][tripleg_id][tripleg_level_item] = tripleg_data
-    return output_offer_level_items, output_tripleg_level_items
-#############################################################################
-#############################################################################
-#############################################################################
 
 app = Flask(__name__)
 @app.route('/compute', methods=['POST'])
@@ -99,28 +82,35 @@ def extract():
         mimetype  ='application/json'
     )
     if VERBOSE== 1:
+        print("______________________________")
         print("price-fc start")
         print("request_id = " + request_id)
-        print("______________________________")
 
+    #
     # I. extract data required by price-fc from cache
-    output_offer_level, output_tripleg_level = extract_data_from_cache(
+    #
+    output_offer_level, output_tripleg_level = cache_operations.extract_data_from_cache(
+        cache,
         request_id,
         ["currency", "bookable_total", "complete_total"],
         ["duration", "can_share_cost"])
 
-    if VERBOSE== 1:
+    if VERBOSE == 1:
         print("output_offer_level   = " + str(output_offer_level))
         print("output_tripleg_level = " + str(output_tripleg_level))
 
+    #
     # II. use the external service to convert prices to EUR
+    #
+
     for offer in output_offer_level["offer_ids"]:
         # complete_total
         output_offer_level[offer]["complete_total"] = price_to_eur(output_offer_level[offer]["currency"], output_offer_level[offer]["complete_total"])
         # boobkable_total
         output_offer_level[offer]["bookable_total"] = price_to_eur(output_offer_level[offer]["currency"], output_offer_level[offer]["bookable_total"])
-
+    #
     # III. compute values assigned to price-fc
+    #
     # compute ticket_coverage and prepare data for calculation of scores
     ticket_coverage      = {}
     offer_bookable_total = {}
@@ -154,7 +144,6 @@ def extract():
     #offer_complete_total_minmax_scores   = normalization.minmaxscore(offer_complete_total, flipped=True)
     #ticket_coverage_minmax_scores        = normalization.minmaxscore(ticket_coverage)
     #can_share_cost_minmax_scores         = normalization.minmaxscore(can_share_cost)
-
     if VERBOSE == 1:
         print("offer_complete_total_z_scores      = " + str(offer_complete_total_z_scores))
         print("ticket_coverage_zscores            = " + str(ticket_coverage_z_scores ))
@@ -164,20 +153,16 @@ def extract():
         #print("ticket_coverage_minmax_scores      = " + str(ticket_coverage_minmax_scores))
         #print("can_share_cost_minmax_scores       = " + str(can_share_cost_minmax_scores))
 
-
+    #
     # IV. store the results produced by price-fc to cache
-    "total_price"
-    for offer in offer_complete_total_z_scores:
-        temp_key = request_id + ":" + offer + ":total_price"
-        cache.set(temp_key, offer_complete_total_z_scores[offer])
-    "ticket_coverage"
-    for offer in ticket_coverage_z_scores:
-        temp_key = request_id + ":" + offer + ":ticket_coverage"
-        cache.set(temp_key, ticket_coverage_z_scores[offer])
-    "can_share_cost"
-    for offer in can_share_cost_z_scores:
-        temp_key = request_id + ":" + offer + ":can_share_cost"
-        cache.set(temp_key, can_share_cost_z_scores[offer])
+    #
+    cache_operations.store_simple_data_to_cache(cache, request_id, offer_complete_total_z_scores, "total_price")
+    cache_operations.store_simple_data_to_cache(cache, request_id, ticket_coverage_z_scores, "ticket_coverage")
+    cache_operations.store_simple_data_to_cache(cache, request_id, can_share_cost_z_scores, "can_share_cost")
+
+    if VERBOSE == 1:
+        print("price-fc end")
+        print("______________________________")
     return response
 #############################################################################
 #############################################################################
@@ -189,12 +174,9 @@ if __name__ == '__main__':
     FLASK_PORT = 5000
     REDIS_HOST = 'localhost'
     REDIS_PORT = 6379
-    VERBOSE    =  1
-
     os.environ["FLASK_ENV"] = "development"
     #cache        = redis.Redis(host=REDIS_HOST, port=REDIS_PORT)
     cache         = rejson.Client(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
-
     print("launching FLASK APP")
     app.run(port=FLASK_PORT, debug=True)
     exit(0)
